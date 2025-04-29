a097ca9d636fec365007893819a6ef3d7ffe5093
EwanEpitech <ewan.tirlemont@epitech.eu>
2025-04-29 17:04:24 +0200

    refactor(main): modularize main function by extracting resource initialization, thread creation, and cleanup into separate functions

--- src/main.c
+++ src/main.c
@@ -12,57 +12,68 @@
 #include "Panoramix/druid.h"
 #include "Panoramix/villager.h"
 
-int main(int ac, char **av)
+static int init_resources(villager_t *args, pthread_t **villagers)
 {
-    args_t args;
-    pthread_t druid;
-    pthread_t *villagers;
-    int i;
-
-    if (parse_args(ac, av, &args) != 0)
-        return 84;
-
-    villagers = malloc(sizeof(pthread_t) * args.nb_villagers);
-    if (!villagers) {
-        free(args.pot);
+    *villagers = malloc(sizeof(pthread_t) * args->nb_villagers);
+    if (!*villagers) {
+        free(args->pot);
         return 84;
     }
+    if (pthread_mutex_init(&args->pot->mutex, NULL) != 0 ||
+        sem_init(&args->pot->refill, 0, 0) != 0) {
+        free(args->pot);
+        free(*villagers);
+        return 84;
+    }
+    return 0;
+}
 
-    if (pthread_mutex_init(&args.pot->mutex, NULL) != 0 ||
-        sem_init(&args.pot->refill, 0, 0) != 0) {
-        free(args.pot);
+static int create_threads(villager_t *args, pthread_t *druid, pthread_t *villagers)
+{
+    if (pthread_create(druid, NULL, druid_thread, args->pot) != 0) {
+        pthread_mutex_destroy(&args->pot->mutex);
+        sem_destroy(&args->pot->refill);
+        free(args->pot);
         free(villagers);
         return 84;
     }
-
-    if (pthread_create(&druid, NULL, druid_thread, args.pot) != 0) {
-        pthread_mutex_destroy(&args.pot->mutex);
-        sem_destroy(&args.pot->refill);
-        free(args.pot);
-        free(villagers);
-        return 84;
-    }
-
-    for (i = 0; i < args.nb_villagers; i++) {
-        if (pthread_create(&villagers[i], NULL, villager_thread, &args) != 0) {
-            pthread_mutex_destroy(&args.pot->mutex);
-            sem_destroy(&args.pot->refill);
-            free(args.pot);
+    for (int i = 0; i < args->nb_villagers; i++) {
+        if (pthread_create(&villagers[i], NULL, villager_thread, args) != 0) {
+            pthread_mutex_destroy(&args->pot->mutex);
+            sem_destroy(&args->pot->refill);
+            free(args->pot);
             free(villagers);
             return 84;
         }
         usleep(1000);
     }
+    return 0;
+}
 
-    for (i = 0; i < args.nb_villagers; i++)
+static void cleanup_resources(villager_t *args, pthread_t *villagers)
+{
+    pthread_mutex_destroy(&args->pot->mutex);
+    sem_destroy(&args->pot->refill);
+    free(args->pot);
+    free(villagers);
+}
+
+int main(int ac, char **av)
+{
+    villager_t args;
+    pthread_t druid;
+    pthread_t *villagers;
+
+    if (parse_args(ac, av, &args) != 0)
+        return 84;
+    if (init_resources(&args, &villagers) != 0)
+        return 84;
+    if (create_threads(&args, &druid, villagers) != 0)
+        return 84;
+    for (int i = 0; i < args.nb_villagers; i++)
         pthread_join(villagers[i], NULL);
-
     pthread_cancel(druid);
     pthread_join(druid, NULL);
-
-    pthread_mutex_destroy(&args.pot->mutex);
-    sem_destroy(&args.pot->refill);
-    free(args.pot);
-    free(villagers);
+    cleanup_resources(&args, villagers);
     return 0;
 }

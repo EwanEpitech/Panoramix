e2e7819f57a7372b7ee49f95d9b3de53d56f57a0
EwanEpitech <ewan.tirlemont@epitech.eu>
2025-04-30 12:49:03 +0200

    docs(panoramix): add detailed documentation for potion and villager structures, and update parse_args function signature to return boolean

--- include/Panoramix/panoramix.h
+++ include/Panoramix/panoramix.h
@@ -11,6 +11,14 @@
     #include <stdbool.h>
     #define PANORAMIX_H
 
+/**
+ * @brief Structure for the potion
+ * @param servings Number of servings left in the pot
+ * @param pot_size Size of the pot
+ * @param refills_left Number of refills left
+ * @param mutex Mutex - Only one thread can access the pot at a time
+ * @param refill Semaphore - Wake up the druid when the pot is empty
+ */
 typedef struct potion_s {
     int servings;
     int pot_size;
@@ -19,6 +27,14 @@ typedef struct potion_s {
     sem_t refill;
 } potion_t;
 
+/**
+ * @brief Structure for the villagers
+ * @param nb_villagers Number of villagers
+ * @param pot_size Size of the pot
+ * @param nb_fights Number of fights left
+ * @param nb_refills Number of refills left
+ * @param pot Pointer to the potion structure
+ */
 typedef struct args_s {
     int nb_villagers;
     int pot_size;
@@ -27,9 +43,13 @@ typedef struct args_s {
     potion_t *pot;
 } villager_t;
 
-void *villager_thread(void *arg);
-void *druid_thread(void *potion);
-
-int parse_args(int ac, char **av, villager_t *args);
+/**
+ * @brief Parse the arguments
+ * @param ac Number of arguments
+ * @param av Array of arguments
+ * @param args Pointer to the villagers structure
+ * @return true on success, false on failure
+ */
+bool parse_args(int ac, char **av, villager_t *args);
 
 #endif
